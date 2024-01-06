2c665bb2bc26376382e7e578b7e0e1bcba8bfe41
Babalo <babalonogqala2003@gmail.com>
2024-01-06 18:42:32 +0200

    add rock paper scissors game

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-let cartQuantity = 0;
+// let cartQuantity = 0;
 
-if (true) {
-  console.log("hello");
-} else {
-  console.log("else");
-}
+// if (true) {
+//   console.log("hello");
+// } else {
+//   console.log("else");
+// }
 
-const age = 15;
+// const age = 15;
 
-// driving age restriction
-if (age >= 18) {
-  console.log("you legally within age");
-} else if (age >= 15) {
-  console.log("you almost theres");
-} else {
-  console.log("you are not allowed to drive");
-}
+// // driving age restriction
+// if (age >= 18) {
+//   console.log("you legally within age");
+// } else if (age >= 15) {
+//   console.log("you almost theres");
+// } else {
+//   console.log("you are not allowed to drive");
+// }
